8aa998e5aa9acbb751cb2e0ed02a62e306f8a6a1
Nikhil Kumar Patra <56184525+NikhilPatra047@users.noreply.github.com>
2024-05-04 22:44:10 +0530

    add types folder

---
+++ src/types/JobCard.ts
@@ -0,0 +1,9 @@
+export type JOB_CARD = {
+  jobTitle: string, 
+  jobRole: string,
+  companyName: string,
+  location: string,
+  jobDescription: string, 
+  expRequired: number | null,
+  logo: string,
+}
