a2163073448cc64ae932a4a91240d94851c94e47
Nikhil Kumar Patra <56184525+NikhilPatra047@users.noreply.github.com>
2024-05-06 13:57:25 +0530

    refactor useGetJobs custom hook

--- src/hooks/useGetJobs.ts
+++ src/hooks/useGetJobs.ts
@@ -1,40 +1,27 @@
 import { useEffect, useState } from "react"
+import { useDispatch } from "react-redux"
 import { JOB_RESULT } from "../types/JobResult"
 import fetchJobs from "../api/fetchJobs"
+import { AppDispatch } from "../redux/store"
+import { failure, initialisation, success } from "../redux/slices/AddApiDataSlice"
 
 export default function useGetJobs(offset: number){
-  const [response, setResponse] = useState<JOB_RESULT[]>([])
-  const [hasMore, setHasMore] = useState<boolean>(false)
-  const [loading, setLoading] = useState<boolean>(false)
-  const [error, setError] = useState<boolean>(false)
-
+  const dispatch = useDispatch<AppDispatch>()
+  
   async function getJobs() {
     const result: { jdList: JOB_RESULT[], totalCount: number } = await fetchJobs(offset)
     return result.jdList
   }
 
   useEffect(() => {
-    setLoading(true)
-    setError(false)
-    setHasMore(false)
-
+    dispatch(initialisation())
     getJobs()
     .then((res) => {
-      setLoading(false)
-      setError(false)
-      setResponse((prevRes: JOB_RESULT[]) => {
-        const newResponse = Array.from(new Set([...prevRes, ...res]))
-        return newResponse
-      })
-      setHasMore(res.length > 0)
+      dispatch(success({ response: res, hasMore: res.length > 0 }))
     })
     .catch((err) => {
-      setError(true)
-      setLoading(false)
-      setHasMore(false)
+      dispatch(failure())
       console.log(err)
     })
   }, [offset])
-
-  return { response, loading, error, hasMore } 
 }
