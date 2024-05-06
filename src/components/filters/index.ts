7b887c2a8eeaa1906576db8a60ed57afaf1a8fb9
Nikhil Kumar Patra <56184525+NikhilPatra047@users.noreply.github.com>
2024-05-06 11:14:57 +0530

    code refactor for easier imports

---
+++ src/components/filters/index.ts
@@ -0,0 +1,6 @@
+import MultiFilter from "./MultiFilter"
+import MultiFilterGrouped from "./MultiFilterGrouped"
+import SingleFilter from "./SingleFilter"
+import TextFilter from "./TextFilter"
+
+export { MultiFilter, MultiFilterGrouped, SingleFilter, TextFilter }
---
+++ src/hooks/index.ts
@@ -0,0 +1,5 @@
+import useFilterJobs from "./useFilterJobs"
+import useGetJobs from "./useGetJobs"
+import useInfiniteScrolling from "./useInfiniteScrolling"
+
+export { useFilterJobs, useGetJobs, useInfiniteScrolling }
