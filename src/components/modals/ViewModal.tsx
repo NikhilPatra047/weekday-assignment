39601b4d66267e0633a37e30e553280614337116
Nikhil Kumar Patra <56184525+NikhilPatra047@users.noreply.github.com>
2024-05-06 13:58:10 +0530

    viewModal: add company name to props

--- src/components/modals/ViewModal.tsx
+++ src/components/modals/ViewModal.tsx
@@ -1,6 +1,6 @@
 import { Box, Modal, Typography } from "@mui/material";
 
-export default function ViewModal({ jobDescription, handleClose, open }: { jobDescription: string, open: boolean, handleClose: () => void }) {
+export default function ViewModal({ companyName, jobDescription, handleClose, open }: { companyName: string, jobDescription: string, open: boolean, handleClose: () => void }) {
     const style = {
         color: 'black',
         position: 'absolute',
@@ -23,10 +23,13 @@ export default function ViewModal({ jobDescription, handleClose, open }: { jobDe
             aria-describedby="modal-modal-description"
         >
             <Box sx={style}>
-                <Typography id="modal-modal-title" style={{ fontSize: '2rem' }} variant="h6" component="h2">
+                <Typography id="modal-modal-title" style={{ fontSize: '2rem' }} sx={{ fontSize: 'bold' }} variant="h6" component="h2">
+                    { companyName }
+                </Typography>
+                <Typography id="modal-modal-title" style={{ fontSize: '1.5rem' }} sx={{ mt: 2}} variant="h6" component="h2">
                     About Company
                 </Typography>
-                <Typography id="modal-modal-description" style={{ fontSize: '1.2rem' }} sx={{ mt: 2 }}>
+                <Typography id="modal-modal-description" style={{ fontSize: '1rem' }} sx={{ mt: 1 }}>
                     { jobDescription }
                 </Typography>
             </Box>
