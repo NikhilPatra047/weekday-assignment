57c3ac98efc9110c60e1753f5fa4bf74a45ce65b
Nikhil Kumar Patra <56184525+NikhilPatra047@users.noreply.github.com>
2024-05-06 11:15:18 +0530

    Search Job Page

--- src/pages/SearchJobPage.tsx
+++ src/pages/SearchJobPage.tsx
@@ -1,39 +1,52 @@
-import { useCallback, useRef, useState } from "react"
 import { JOB_RESULT } from "../types/JobResult"
 import JobCard from "../components/cards/JobCard"
-import useGetJobs from "../hooks/useGetJobs"
+import { roles, experience, location, minBasePay, LOCATION, PAY, EXPERIENCE } from "../../data/index.ts"
+import { MultiFilter, MultiFilterGrouped, SingleFilter, TextFilter } from "../components/filters/index.ts"
+import { useFilterJobs, useGetJobs, useInfiniteScrolling } from "../hooks/index.ts"
+import { useSelector } from "react-redux"
+import { RootState } from "../redux/store.js"
 
 export default function SearchJobPage() {
-  const [pageNumber, setPageNumber] = useState<number>(0)
-  const { response, loading, error, hasMore } = useGetJobs(pageNumber)
+  const state = useSelector((state: RootState) => state.filter)
+  const { loading, error, pageNumber } = useSelector((state: RootState) => state.apiData)
 
-  const observer = useRef<IntersectionObserver>()
-  const lastJobCard = useCallback((node: HTMLDivElement) => {
-    if (loading) return
-    observer.current = new IntersectionObserver(entries => {
-      if (entries[0].isIntersecting && hasMore) {
-        setPageNumber((prevPageNumber: number)=> {
-          const limit: number = Number(import.meta.env.VITE_LIMIT)
-          return limit + prevPageNumber
-        })
-      }
-    })
-    if (observer.current) observer.current?.disconnect()
-    if (node) observer.current?.observe(node)
-  }, [loading, hasMore])
+  const { response } = useGetJobs(pageNumber)
+  const { lastJobCard } = useInfiniteScrolling()
+  const { filteredResponse } = useFilterJobs(response)
 
   return (
-    <section className='cardSection'>
-      {response.map((resp: JOB_RESULT, index: number) => {
-        if (response.length === index + 1) {
-          return <div key={`${resp.jdUid}`} ref={lastJobCard}>
-            <JobCard jobTitle="" jobDescription={resp.jobDetailsFromCompany? resp.jobDetailsFromCompany: ''} expRequired={resp.minExp? resp.minExp: null} logo={resp.logoUrl? resp.logoUrl: ''} companyName={resp.companyName? resp.companyName: ''} jobRole={resp.jobRole? resp.jobRole: ''} location={resp.location? resp.location: ''} />
-          </div>
+    <section>
+      <div className='filters'>
+          <MultiFilterGrouped menu={roles} label={'Roles'} />
+          <MultiFilter type={LOCATION} menu={location} label={'Location'} />
+          <SingleFilter type={EXPERIENCE} menu={experience} label={'Experience'} />
+          <SingleFilter type={PAY} menu={minBasePay} label={'Min Base Pay'} />
+          <TextFilter label={'Search by company'} />
+      </div>
+      <div className='cardSection'>
+        {
+          (state.location.length !== 0 || state.experience !== null || state.minBasePay !== null || state.search !== '' || state.roles.length !== 0)
+          ? (filteredResponse.map((resp: JOB_RESULT) => {
+            return (
+              <JobCard key={`${resp.jdUid}`} link={resp.jdLink? resp.jdLink: ''} jobTitle="" jobDescription={resp.jobDetailsFromCompany? resp.jobDetailsFromCompany: ''} expRequired={resp.minExp? resp.minExp: null} logo={resp.logoUrl? resp.logoUrl: ''} companyName={resp.companyName? resp.companyName: ''} jobRole={resp.jobRole? resp.jobRole: ''} location={resp.location? resp.location: ''} maxPay={resp.maxJdSalary? resp.maxJdSalary: null} minPay={resp.minJdSalary? resp.minJdSalary: null} />
+            )
+          }))
+          : (response.map((resp: JOB_RESULT, index: number) => {
+            if (response.length === index + 1) {
+              return <div key={`${resp.jdUid}`} ref={lastJobCard}>
+                <JobCard jobTitle="" link={resp.jdLink? resp.jdLink: ''} jobDescription={resp.jobDetailsFromCompany? resp.jobDetailsFromCompany: ''} expRequired={resp.minExp? resp.minExp: null} logo={resp.logoUrl? resp.logoUrl: ''} companyName={resp.companyName? resp.companyName: ''} jobRole={resp.jobRole? resp.jobRole: ''} location={resp.location? resp.location: ''} maxPay={resp.maxJdSalary? resp.maxJdSalary: null} minPay={resp.minJdSalary? resp.minJdSalary: null} />
+              </div>
+            }
+            return (
+              <JobCard key={`${resp.jdUid}`} link={resp.jdLink? resp.jdLink: ''} jobTitle="" jobDescription={resp.jobDetailsFromCompany? resp.jobDetailsFromCompany: ''} expRequired={resp.minExp? resp.minExp: null} logo={resp.logoUrl? resp.logoUrl: ''} companyName={resp.companyName? resp.companyName: ''} jobRole={resp.jobRole? resp.jobRole: ''} location={resp.location? resp.location: ''} maxPay={resp.maxJdSalary? resp.maxJdSalary: null} minPay={resp.minJdSalary? resp.minJdSalary: null} />
+            )
+          }))
         }
-        return (
-          <JobCard key={`${resp.jdUid}`} jobTitle="" jobDescription={resp.jobDetailsFromCompany? resp.jobDetailsFromCompany: ''} expRequired={resp.minExp? resp.minExp: null} logo={resp.logoUrl? resp.logoUrl: ''} companyName={resp.companyName? resp.companyName: ''} jobRole={resp.jobRole? resp.jobRole: ''} location={resp.location? resp.location: ''} />
-        )
-      })}
+      </div>
+      { 
+        filteredResponse.length === 0 && (state.location.length !== 0 || state.experience !== null || state.minBasePay !== null || state.search !== '' || state.roles.length !== 0)
+        && <p style={{ marginTop: '2rem', fontSize: '1.5rem', color: 'black', width: 'fit-content', margin: '0 auto',  }}>No job matched your search.</p>
+      }
       <div>{ loading && '...Loading' }</div>
       <div>{ error && `${error}` }</div>
     </section>
